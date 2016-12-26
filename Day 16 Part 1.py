ff5711fd77b2d87c22e2c04eb939559a9fbcb9b1
Tobias Lindblad <tobias.gbg@gmail.com>
2016-12-26 09:10:23 +0100

    now it fits on one page

--- Day 16 Part 1.py
+++ Day 16 Part 1.py
@@ -5,12 +5,10 @@ from builtins import reversed
 def generate_data(data):
     a = data
     b = copy.deepcopy(a)
-
     b = ''.join(reversed(b))
     b = b.replace('1', '7')
     b = b.replace('0', '1')
     b = b.replace('7', '0')
-
     return a + '0' + b
 
 def get_checksum(data):
@@ -22,7 +20,6 @@ def get_checksum(data):
         else:
             checksum += '0'
         index += 2
-
     if len(checksum) % 2 == 0:
         return get_checksum(checksum)
     else:
@@ -32,11 +29,9 @@ size_to_fill = 35651584
 input = '10111011111001111'
 data = generate_data(input)
 checksum = get_checksum(data)
-
 while (len(data) < size_to_fill):
     data = generate_data(data)
     if (len(data) > size_to_fill):
         data = data[0:size_to_fill]
     checksum = get_checksum(data)
-
 print ('checksum: ' + checksum)
